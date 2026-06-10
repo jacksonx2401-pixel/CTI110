06e09745cd37761065a5cbee15735bb57227212a
jacksonx2401-pixel <jacksonx2401@student.faytechcc.edu>
2026-06-10 09:03:54 -0400

    Add files via upload

---
+++ P1LAB1_Jackson_Ximorra.py
@@ -0,0 +1,11 @@
+# Ximora Jackson
+# 06/10/26
+# P1LAB1
+#This project is learning about how to put input and display output into a python program.
+
+user_name = input()
+print("Hello, " + user_name," and Welcome to CTI-110.")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello," + first_name + " " + last_name + "! Welcome to CTI-110")  
